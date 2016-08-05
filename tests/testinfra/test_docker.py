4340d237e96a3942ae6b4deddcb0327521a30b4a
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-08-05 16:29:58 +0200

    Add more tests

--- tests/testinfra/test_docker.py
+++ tests/testinfra/test_docker.py
@@ -7,9 +7,8 @@ import pytest
 # pytestmark = pytest.mark.destructive
 
 # To run all the tests on given docker images:
-pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh')
-#pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh',
-#                                       'infopen/ubuntu-xenial-ssh')
+pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh',
+                                       'infopen/ubuntu-xenial-ssh-py27')
 
 # Both
 # pytestmark = [
@@ -18,8 +17,13 @@ pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh')
 # ]
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+def test_packages(Package):
+    assert Package('collectd').is_installed
 
-def test_foo_b(User):
-    assert User().name == 'root'
+
+def test_service(Service):
+    assert Service('collectd').is_enabled
+    assert Service('collectd').is_running
+
+def test_process(Process):
+    assert len(Process.filter(user='root', comm='collectd')) == 1
