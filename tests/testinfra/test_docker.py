331e1bed5e5939f91d6bfcb7283c36fe2ac4a001
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-08-06 03:09:49 +0200

    Add collectd configuration management

--- tests/testinfra/test_docker.py
+++ tests/testinfra/test_docker.py
@@ -18,12 +18,33 @@ pytestmark = pytest.mark.docker_images('infopen/ubuntu-trusty-ssh',
 
 
 def test_packages(Package):
+    """
+    Test packages install
+    """
     assert Package('collectd').is_installed
 
 
 def test_service(Service):
+    """
+    Test service settings
+    """
     assert Service('collectd').is_enabled
     assert Service('collectd').is_running
 
 def test_process(Process):
+    """
+    Test process running
+    """
     assert len(Process.filter(user='root', comm='collectd')) == 1
+
+def test_config_files(File):
+    """
+    Test config files exists
+    """
+    config_files = ['/etc/collectd/collectd.conf', '/etc/collectd/my_types.db']
+    for current_file in config_files:
+        File(current_file).exists
+        File(current_file).is_file
+        File(current_file).user == 'root'
+        File(current_file).group == 'root'
+        File(current_file).mode == 0o0400
